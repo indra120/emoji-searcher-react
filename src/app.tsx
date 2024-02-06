b59064fe1030ce2d879e20bb5e0cbd70880b7b0d
indra120 <indrapranata120@gmail.com>
2024-02-06 10:36:44 +0800

    Fetching list of emoji

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "./components/header"
 
+const API_URL = "https://run.mocky.io/v3/5a982f64-218d-45d7-a380-ebe924d55631"
+
+interface Emoji {
+  title: string
+  symbol: string
+  keywords: string
+}
+
 const App = () => {
+  const [emojiList, setEmojiList] = useState<Emoji[]>([])
   const [search, setSearch] = useState<string>("")
+
+  useEffect(() => {
+    ;(async function () {
+      const res = await fetch(API_URL)
+      const resJson: Emoji[] = await res.json()
+
+      setEmojiList(resJson)
+      console.log(emojiList[0])
+    })()
+  }, [])
 
   return (
     <>
