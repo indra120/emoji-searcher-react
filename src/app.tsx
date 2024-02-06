55d0ce4d8a6e12c878a665a7118ddace6af44059
indra120 <indrapranata120@gmail.com>
2024-02-06 10:18:14 +0800

    Search bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import Header from "./components/header"
 
 const App = () => {
+  const [search, setSearch] = useState<string>("")
+
   return (
     <>
       <Header />
 
-      <main className="container"></main>
+      <main className="container">
+        <input
+          type="text"
+          className="search"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </main>
     </>
   )
 }
